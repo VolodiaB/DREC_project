d144518459b30ee14c062054815a872e601096a5
GhostWK <Zubkov.AS@phystech.edu>
2017-02-20 13:28:28 +0300

    Window and Start classes

---
+++ src/engine/window/Window.java
@@ -0,0 +1,30 @@
+package engine.window;
+
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+
+/**
+ * Created by USER on 20.02.2017.
+ */
+public class Window extends BasicGame{
+    public Window(String title) {
+        super(title);
+    }
+
+    @Override
+    public void init(GameContainer gameContainer) throws SlickException {
+
+    }
+
+    @Override
+    public void update(GameContainer gameContainer, int i) throws SlickException {
+
+    }
+
+    @Override
+    public void render(GameContainer gameContainer, Graphics graphics) throws SlickException {
+
+    }
+}
---
+++ src/game/Start.java
@@ -0,0 +1,27 @@
+package game;
+
+import engine.window.Window;
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.SlickException;
+
+/**
+ * Created by USER on 20.02.2017.
+ */
+public class Start {
+    public static final String TITLE = "Game";
+    public static final int HEIGHT = 720;
+    public static final int WIDTH = 1280;
+    public static final boolean IS_FULLSCREEN = false;
+
+
+    private static AppGameContainer appgc;
+    public static void main(String[] args) {
+        try {
+            appgc = new AppGameContainer(new Window(TITLE));
+            appgc.setDisplayMode(WIDTH,HEIGHT,IS_FULLSCREEN);
+            appgc.start();
+        } catch (SlickException e) {
+            e.printStackTrace();
+        }
+    }
+}
